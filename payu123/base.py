a20ca8afeca2f9189494890e448fbcb0a2cd96ff
jefree <jgarzon920429@gmail.com>
2015-01-11 15:32:10 -0500

    fix error on send APIRequest

--- payu123/base.py
+++ payu123/base.py
@@ -3,6 +3,6 @@ import requests, json
 
 class APIRequest():
 
-    def send(url, data):
+    def send(self, url, data):
         resp = requests.post(url, data=json.dumps(data), headers={'content-type': 'application/json', 'accept': 'application/json'}, verify=settings.SSL_VERIFY)
         return resp.json()
